e3292c8eeee382cd8979f904d715bdb79125f31b
LittleQi <123362884+litterqi@users.noreply.github.com>
2023-10-20 19:00:25 +0800

    Update 2.py

--- 作业06/2.py
+++ 作业06/2.py
@@ -3,9 +3,11 @@ import matplotlib.pyplot as plt
 # 生成100个服从标准正态分布的样本
 samples = np.random.normal(0, 1, 100)
 # 绘制直方图
-plt.hist(samples, bins=20, edgecolor='black')
+plt.hist(samples, bins=20,density=True,edgecolor='black')
 plt.xlabel('Value')
 plt.ylabel('Frequency')
 plt.title('Histogram of Samples')
+# 绘制分布函数
+plt.plot(np.sort(samples), 1/(np.sqrt(2 * np.pi)) * np.exp(- (np.sort(samples))**2 /2), color='black')
 # 显示图表
 plt.show()
